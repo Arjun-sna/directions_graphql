a179ad7d4c5b7047990ef85cae72f4e54b6e1b00
Arjun sn <snarjun92@gmail.com>
2019-06-11 12:14:56 +0530

    :art: restructure schema

---
+++ src/schema/users/index.js
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-server';
+
+module.exports = gql`
+  extend type Query {
+    users: [User]!
+  }
+
+  extend type Mutation {
+    updateUser(username: String): Boolean!
+    deleteUser(id: ID!): Boolean!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+  }
+`
